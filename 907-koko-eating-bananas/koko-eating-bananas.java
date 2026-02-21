0619bc1d676f4364d2ad7ae15580dc18389ade06
Jayshree005 <2300301540044@ipec.org.in>
2026-02-21 13:03:09 +0530

    Time: 12 ms (56.02%) | Memory: 48.7 MB (9.84%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static boolean KyaBanana(int mid, int[]piles,int h)
+    {
+        long totalTime=0;
+
+        for(int i =0;i<piles.length;i++)
+        {
+            totalTime += piles[i]/mid;
+            if(piles[i]%mid !=0){
+                totalTime++ ;
+            }
+        }
+        return totalTime<=h; // boolean value return krta hai
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=0;
+        for(int banana : piles)
+        {
+            high=Math.max(high,banana);
+        }
+        int ans=0;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            if (KyaBanana(mid,piles,h))
+            {
+                ans = mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }       
+            
+        }
+        return ans;
+        
+    }
+}
