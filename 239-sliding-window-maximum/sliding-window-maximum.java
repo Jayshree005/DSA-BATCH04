a3b14715592e5107ceca7f0c3d19d98c4e8eca06
Jayshree005 <2300301540044@ipec.org.in>
2026-02-27 12:42:23 +0530

    Time: 502 ms (5.00%) | Memory: 152.9 MB (26.89%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int i =0, j=0, n=nums.length , maxEle=-9999;
+        int[]ans= new int [n-k+1];
+        TreeMap<Integer,Integer>map=new TreeMap<>();
+        int count=0;
+        if ( n==1 || k==1) return nums;
+        while (j<n){
+            map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+            maxEle=Math.max(nums[j],maxEle);
+            //shrinking
+            if (j-i+1>k){
+                map.put(nums[i],map.getOrDefault(nums[i],0)-1) ;
+                if(map.get(nums[i]) == 0){
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            if (j-i+1==k){
+                ans[count]=map.lastKey();
+                count++;
+            }
+            j++;
+        }
+        return ans;
+        
+    }
+}
