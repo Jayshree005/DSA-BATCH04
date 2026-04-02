791d6fcf4dc74f379ca480019c4a079438facc56
Jayshree005 <2300301540044@ipec.org.in>
2026-04-02 12:09:40 +0530

    Time: 9 ms (25.46%) | Memory: 65.3 MB (49.00%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,38 +1,41 @@
-import java.util.*;
-
 class Solution {
-
+    public static boolean isPalindrome(String s)
+    {
+        int i = 0 , j = s.length()-1 ;
+        while(i < j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+            {
+                return false ;
+            }
+            i++ ;
+            j-- ;
+        }
+        return true ;
+    }
+    public static void Solve(int i , String s , List<List<String>> ans, List<String> temp)
+    {
+        if(i == s.length())
+        {
+            ans.add(new ArrayList(temp)) ;
+            return ;
+        }
+        for(int j = i ; j < s.length() ; j++)
+        {
+             if(isPalindrome(s.substring(i,j+1)))
+             {
+                temp.add(s.substring(i, j+1)) ;;
+                Solve(j+1,s,ans,temp) ;
+                temp.remove(temp.size()-1) ;
+             }
+        }
+    }
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(0, s, new ArrayList<>(), result);
-        return result;
-    }
-    private void backtrack(int start, String s, List<String> curr, List<List<String>> result) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(curr));
-            return;
-        }
-
-        for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
-                curr.add(s.substring(start, end + 1));
-                backtrack(end + 1, s, curr, result);
-                curr.remove(curr.size() - 1);
-            }
-        }
-    }
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.partition("aab"));
+        
+        List<List<String>> ans = new ArrayList() ;
+        List<String> temp = new ArrayList() ;
+     
+        Solve(0,s,ans,temp) ;
+        return ans ;
     }
 }
